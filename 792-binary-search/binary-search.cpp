531290c5a53cc9368cd7bdad4002e4387b79c44d
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-02-02 18:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (44.37%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,20 +1,23 @@
+int binarySearchRecursion(vector<int>& nums,int low,int high, int target){
+    if(low>high){
+        return -1;
+    }
+    int mid = (low+high)/2;
+    if(target==nums[mid]){
+        return mid;
+    }
+    if(target>nums[mid]){
+        return binarySearchRecursion(nums,mid+1,high,target);
+    }
+    else{
+        return binarySearchRecursion(nums,low,mid-1,target);
+    }
+
+}
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(target>nums[mid]){
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
-            }
-        }
-        return -1;
+        int low = 0,high = nums.size()-1;
+        return binarySearchRecursion(nums,low,high,target);
     }
 };
