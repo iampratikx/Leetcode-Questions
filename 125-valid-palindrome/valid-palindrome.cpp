b7b0b5af41d1eb643ccff0766ae6cac2abd2e822
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-02-27 14:58:02 +0530

    Time: 5 ms (8.42%) | Memory: 10.8 MB (5.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,21 +1,46 @@
-bool palindrom(int i,string &s,int n){
-    if(i>=n/2){
+class Solution {
+private:
+    bool valid(char s){
+        if((s >='a' && s <='z' ) || (s >='A' && s <='Z') || (s >='0' && s <='9')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    char uperTolower(char ch){
+        if((ch >= 'a' && ch <= 'z') || (ch >='0' && ch <= '9')){
+            return ch;
+        }
+        else{
+            char temp  = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+    bool Palindrome(string temp){
+        int s = 0;
+        int e = temp.length() - 1;
+        while(s<e){
+            if(temp[s] != temp[e]){
+                return false;
+            }
+            else{
+                s++,e--;
+            }
+        }
         return true;
     }
-    if(s[i] != s[n-i-1]){
-        return false;;
-    }
-    return palindrom(i+1,s,n);
-}
-class Solution {
 public:
     bool isPalindrome(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        s.erase(remove_if(s.begin(), s.end(),
-                      [](char c) { return !isalnum(c); }),
-            s.end());
-
-        int n = s.size();
-        return palindrom(0,s,n);
+        string temp = "";
+        for(int i = 0; i<s.length(); i++){
+            if(valid(s[i])){
+                temp.push_back(s[i]);
+            }
+        }
+        for(int i = 0; i<temp.length(); i++){
+            temp[i] = uperTolower(temp[i]);
+        }
+        return Palindrome(temp);
     }
 };
