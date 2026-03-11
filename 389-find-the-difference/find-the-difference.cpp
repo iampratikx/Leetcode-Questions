11eb8809ab7d5e8ca43f6f43bc60f8734ff5dc57
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-03-12 00:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.91%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result = 0;
+        for(char ch : s){
+            result = result ^ ch;
+        }
+        for(char ch : t){
+            result = result ^ ch;
+        }
+        return result;
+    }
+};
