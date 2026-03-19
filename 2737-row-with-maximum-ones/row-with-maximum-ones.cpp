92b2e4800f7d1ef61d5066312cf7a98b7e05a9c2
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-03-19 21:40:58 +0530

    Time: 7 ms (30.27%) | Memory: 64.8 MB (10.61%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,4 +1,20 @@
 class Solution {
+private:
+    int lowerbound(vector<int> &arr,int n, int x){
+        sort(arr.begin(),arr.end());
+        int low = 0, high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = low + (high -low)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
 
@@ -10,19 +26,12 @@ public:
 
         for(int i = 0; i < n; i++){
             
-            int countOnes = 0;
-            
-            for(int j = 0; j < m; j++){
-                if(mat[i][j] == 1)
-                    countOnes++;
-            }
-
+            int countOnes = m - lowerbound(mat[i],m,1);
             if(countOnes > maxCount){
                 maxCount = countOnes;
                 rowIndex = i;
             }
         }
-
         return {rowIndex, maxCount};
     }
 };
