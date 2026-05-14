f19bd63476393894afa2e59f271f1bd0c655bb1b
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-05-14 13:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (92.53%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,4 +1,30 @@
 class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        int l = 0, r = n-1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+
+        while(l < r){
+            lmax = max(lmax,h[l]);
+            rmax = max(rmax,h[r]);
+
+            if(lmax < rmax){
+                ans += lmax - h[l];
+                l++;
+            }
+            else{
+                ans += rmax - h[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
+
+/* 
+class Solution {
 private:
     vector<int> leftMax(vector<int>& height,int n){
         vector<int> lMax(n);
@@ -36,3 +62,4 @@ public:
         return sum;
     }
 };
+*/
