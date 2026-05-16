a8d4f56fafc244a5b724a697f072a297a4a0bcf9
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-05-16 14:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.17%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,24 +1,61 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0, n = s.length();
-        // 1. Skip leading spaces
-        while (i < n && s[i] == ' ') i++;
-        // 2. Handle sign
+
+        if(s.length() == 0) return 0;
+
+        int i = 0;
+
+        // remove spaces
+        while(i < s.size() && s[i] == ' ') {
+            i++;
+        }
+
+        s = s.substr(i);
+
+        if(s.length() == 0) return 0;
+
         int sign = 1;
-        if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') sign = -1;
-            i++;
-        }
-        // 3. Convert digits
         long result = 0;
-        while (i < n && s[i] >= '0' && s[i] <= '9') {
-            result = result * 10 + (s[i] - '0');     
-            // 4. Handle overflow
-            if (sign * result > INT_MAX) return INT_MAX;
-            if (sign * result < INT_MIN) return INT_MIN;
+
+        i = 0;
+
+        // sign handling
+        if(s[0] == '-') {
+            sign = -1;
             i++;
         }
-        return sign * result;
+        else if(s[0] == '+') {
+            i++;
+        }
+
+        // skip leading zeroes
+        while(i < s.size() && s[i] == '0') {
+            i++;
+        }
+
+        // number conversion
+        if(i < s.size() && s[i] >= '0' && s[i] <= '9') {
+
+            while(i < s.size() && s[i] >= '0' && s[i] <= '9') {
+
+                int num = s[i] - '0';
+
+                result = result * 10 + num;
+
+                // overflow handling
+                if(sign == 1 && result > INT_MAX)
+                    return INT_MAX;
+
+                if(sign == -1 && -result < INT_MIN)
+                    return INT_MIN;
+
+                i++;
+            }
+
+            return result * sign;
+        }
+
+        return 0;
     }
 };
