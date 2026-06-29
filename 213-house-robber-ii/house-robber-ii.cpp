62cf2eac31efa9078f0449207a379e331a9b9c1d
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-06-30 00:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (62.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,35 +1,32 @@
 class Solution {
-public:
-    vector<int> dp;
-    int helper(vector<int> & nums, int i){
-        if(i>=nums.size()){
-            return 0;
-        }
-        if(dp[i] != -1) return dp[i];
-        int take = nums[i] + helper(nums,i+2);
-        int leave = helper(nums,i+1);
+    int solve(vector<int>& nums) {
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1; i<nums.size(); i++){
+            int pick = nums[i];
+            if(i > 1) pick += prev2;
+            int notpick = 0 + prev;
 
-        return dp[i] = max(take,leave);
+            int curri = max(pick, notpick);
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
     }
+public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n+1,-1);
         vector<int > temp1;
         vector<int > temp2;
         if(n == 1) return nums[0];
         for(int i = 0; i < n; i++){
             if(i != 0)
-                temp1.push_back(nums[i]);   // first house remove
-
+                temp1.push_back(nums[i]);
             if(i != n-1)
-                temp2.push_back(nums[i]);   // last house remove
+                temp2.push_back(nums[i]);
         }
-        dp.assign(temp1.size(), -1);
-        int ans1 = helper(temp1,0);
-
-        dp.assign(temp2.size(), -1);
-        int ans2 = helper(temp2,0);
-
+        int ans1 = solve(temp1);
+        int ans2 = solve(temp2);
         return max(ans1, ans2);
     }
 };
