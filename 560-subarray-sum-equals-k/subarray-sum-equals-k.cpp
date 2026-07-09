55ddf20b7788b9ea359bb45135c9b55fa1a0fbfc
Pratik Chavan <147658734+iampratikx@users.noreply.github.com>
2026-07-10 00:35:13 +0530

    Time: 43 ms (58.84%) | Memory: 45.5 MB (56.36%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int,int> mpp;
-        mpp[0] = 1;
-        int preSum = 0, cnt =0;
-        for(int i=0;i<nums.size();i++){
-            preSum +=nums[i];
-            int remove = preSum-k;
-            cnt+=mpp[remove];
-            mpp[preSum]+=1;
+
+        unordered_map<int, int> mp;
+
+        // Prefix sum 0 ek baar already exist karta hai
+        mp[0] = 1;
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            prefixSum += nums[i];
+
+            // Agar (prefixSum - k) pehle mila hai,
+            // to utni subarrays mil jayengi
+            if (mp.find(prefixSum - k) != mp.end()) {
+                count += mp[prefixSum - k];
+            }
+
+            // Current prefix sum store karo
+            mp[prefixSum]++;
         }
-        return cnt;
+
+        return count;
     }
 };
